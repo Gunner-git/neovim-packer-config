29dd969b992f6e91444b1dd8fc87280414a827df
Gunner-git <gurnoorjagjotsingh@gmail.com>
2024-07-16 01:58:28 +0530

    Packer neovim basic config

---
+++ after/plugin/colors.lua
@@ -0,0 +1,19 @@
+function ColorMyGruvbox(color)
+	color = color or "gruvbox"
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+end
+
+function ColorMyRosepine(color)
+	color = color or "rose-pine"
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+end
+
+ColorMyGruvbox()
---
+++ lua/gursin/init.lua
@@ -0,0 +1,13 @@
+require("gursin.remap")
+
+vim.opt.swapfile = false
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.updatetime = 50
